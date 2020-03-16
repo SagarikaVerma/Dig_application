c0367e3172500fafc4e1b94067a41fc035bea553
Neha Kulkarni <nehawcbloom04@gmail.com>
2020-03-16 18:12:28 +0530

    tests files

--- dig_sig/auth.py
+++ dig_sig/auth.py
@@ -78,7 +78,7 @@ def load_logged_in_user():
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for('hello'))
 
 
 def login_required(view):
