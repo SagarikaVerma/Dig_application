02117ecacacf6de2ef32a4cd04d01bfa96876bb9
Neha Kulkarni <nehawcbloom04@gmail.com>
2020-03-20 22:43:16 +0530

    schema update
    schema with user table, application data and signatories table

--- dig_sig/schema.sql
+++ dig_sig/schema.sql
@@ -1,17 +1,28 @@
 DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS application_data;
+DROP TABLE IF EXISTS signatories_table;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+  password TEXT NOT NULL,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL
 );
 
-CREATE TABLE post (
+CREATE TABLE application_data (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   author_id INTEGER NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   title TEXT NOT NULL,
   body TEXT NOT NULL,
   FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+CREATE TABLE signatories_table (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	sig_name TEXT NOT NULL,
+	designation TEXT NOT NULL,
+	institute_name TEXT NOT NULL,
+	gender TEXT NOT NULL
 );
